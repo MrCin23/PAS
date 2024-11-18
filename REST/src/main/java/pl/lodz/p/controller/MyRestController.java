b18662a7f10e2393936d63e508437200b845f7fa
MrCin23 <marcin.targonski23@gmail.com>
2024-11-18 17:49:00 +0100

    Spring nie działa

---
+++ REST/src/main/java/pl/lodz/p/controller/MyRestController.java
@@ -0,0 +1,15 @@
+package pl.lodz.p.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, world!";
+    }
+}
