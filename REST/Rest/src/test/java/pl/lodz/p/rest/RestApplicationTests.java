298013cb06125d962dc1c9a6002a7d4680b5163d
MrCin23 <marcin.targonski23@gmail.com>
2024-11-18 17:02:07 +0100

    xD

---
+++ REST/Rest/src/test/java/pl/lodz/p/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
